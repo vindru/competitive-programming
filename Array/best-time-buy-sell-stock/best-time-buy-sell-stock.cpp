4e86f30300a67a5dc27c1b15587a238e5f4a2d74
Rajat Deepak Vindru <rajatvindr000@gmail.com>
2022-01-28 08:31:13 +0530

    best-time-to-buy-and-sell-stock

---
+++ Array/best-time-buy-sell-stock/best-time-buy-sell-stock.cpp
@@ -0,0 +1,40 @@
+/*
+Author: Vindru
+Problem: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+Logic: complexity O(n), keep track of profit all time
+*/
+
+class Solution
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        int minEle = prices[0];
+        int maxEle = prices[0];
+        int profit = 0;
+        if (prices.size() == 1)
+        {
+            return 0;
+        }
+
+        for (int i = 1; i < prices.size(); i++)
+        {
+            if (prices[i] > maxEle)
+            {
+                maxEle = prices[i];
+                int p = maxEle - minEle;
+                if (p > profit)
+                {
+                    profit = p;
+                }
+            }
+            else if (prices[i] < minEle)
+            {
+                minEle = prices[i];
+                maxEle = prices[i];
+            }
+        }
+        return profit;
+    }
+};
